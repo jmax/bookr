888c10d4dcb5a8edb146f3bbe82bf888ce44ba9b
Juan María Martínez Arce <juan@insignia4u.com>
2010-07-06 16:28:54 -0300

    [Refactoring]
      - Pequeña correccion en el controller books.

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -2,8 +2,7 @@ class Admin::BooksController < Admin::TagsController
   administrate_me do |admin|
     admin.search 'books.title', 'books.abstract'
     admin.order 'books.published_on DESC, books.title'
-    admin.has_and_belongs_to_many :authors
-    admin.has_and_belongs_to_many :tags
+    admin.has_and_belongs_to_many :authors, :tags
     admin.filters do |filter|
       filter.combo :category_id do
         Category.to_select
