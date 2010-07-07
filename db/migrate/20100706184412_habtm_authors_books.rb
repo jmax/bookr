88d3e924b504c70fa163a022812389ee84f81e6e
Juan María Martínez Arce <juan@insignia4u.com>
2010-07-07 16:55:47 -0700

    Agrego documentacion para una migracion habtm

--- db/migrate/20100706184412_habtm_authors_books.rb
+++ db/migrate/20100706184412_habtm_authors_books.rb
@@ -1,3 +1,9 @@
+# Esta migracion se genero con:
+# ruby script/generate migration habtm_authors_books
+#
+# Es para solucionar una relacion M a N entre los 
+# modelos author y book
+#
 class HabtmAuthorsBooks < ActiveRecord::Migration
   def self.up
     create_table :authors_books, :id => false do |t|
